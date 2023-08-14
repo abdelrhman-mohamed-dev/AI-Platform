5a205219edaf6204443ae9c4343957c41819ab0b
boody-cpu <81966035+boody-cpu@users.noreply.github.com>
2023-08-15 00:00:03 +0300

    dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,87 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import {
+  ArrowRight,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon,
+} from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Card } from "@/components/ui/card";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    href: "/conversation",
+    bgColor: "bg-pink-700/10",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    herf: "/video",
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    herf: "/music",
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    herf: "/code",
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+  },
+];
 
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <div>
-      <p>Dashboard (protected)</p>
-      
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl text-center font-bold">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => {
+              router.push(tool.herf!);
+            }}
+            key={tool.herf}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn(tool.bgColor, "p-2 w-fit rounded-md")}>
+                <tool.icon className={cn(tool.color, "w-8 h-8")} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -13,6 +13,7 @@ import {
   Settings,
   VideoIcon,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -61,10 +62,11 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathName = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white ">
       <div className="px-3 py-2 flex-1">
-        <Link href={"/dashboard"} className="flex items-center pl-3 mb-14">
+        <Link href={"/dashboard"} className="flex items-center pl-3 mb-8">
           <div className="relative w-8 h-8 mr-4">
             <Image fill alt="logo" src="/logo.png" />
           </div>
@@ -77,7 +79,12 @@ const Sidebar = () => {
             <Link
               href={route.herf}
               key={route.herf}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                pathName === route.herf
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400",
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              )}
             >
               <div className="flex items-center flex-1 ">
                 <route.icon className={cn(route.color, "h-5 w-5 mr-3")} />
